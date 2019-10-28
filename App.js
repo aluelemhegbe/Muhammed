e9ccd41c9bcc033de4fdaa303c74efa75e06041f
aluelemhegbe <mijinyawa@gmail.com>
2019-10-28 15:29:27 +0100

    project upload

---
+++ App.js
@@ -0,0 +1,39 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return(
+            <div className="App">)
+                <header className="App-header'>
+               <img src={logo} className="App-logo" alt="logo" />
+              <p>
+                Edit <code>src/App.js</code> and save to reload.
+               </p>
+               <a
+                ClassName="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+                >
+                try {
+                      // here place your campaign logic
+                      console.log('Campaign Successfully Created! You did it!: ', Continue)
+                    } catch (err) {
+                      console.log('error creating campaign: ', err)
+                    }
+               };
+              }
+             }
+             export default App;
+ 
